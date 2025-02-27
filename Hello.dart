7f346190aedf03cad3b1c63d058ce5cf1ad74448
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-02-27 19:22:39 +0300

    Comments in dart programming language

--- Hello.dart
+++ Hello.dart
@@ -3,3 +3,18 @@
 main() {
   print("Hello Muhammed");
 }
+
+// This single line comment
+
+/// This single line documentation comment
+
+
+/*
+This is
+block comment
+ */
+
+/**
+ * This is block documentation comment
+ * "documentation comments are used to tell the about our code that can be processed by the ide for someone else "
+ */
