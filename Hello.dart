2e3dec1f6b6869e4fffff77b901035567966504a
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-02-28 00:54:55 +0300

    Difference between Mutable and Immutable object

--- Hello.dart
+++ Hello.dart
@@ -1,18 +1,13 @@
 /// variable in dart
 
 void main() {
-  // every literal is a tye of supper class "Object"
-  // by default every custom and builtin classes
-  // are subclass of Object class which contains
-  // runtimetype, hashcode, toString(), ==
-  // and Static methods hash(), hashAll()
+  /// Difference between Mutable and immutable object
+  /// Mutable objects: these are type of object that
+  ///   -> change their internal state
+  ///   -> sed for date state is variable for some data processing
+  /// Immutable objects: these are type of objects that
+  ///   -> doesn't change its internal state after thy are being initialized
+  ///   -> used for data safety, consistency and thread safe data manipulation
 
-  Object  obj = 1;
-  print(obj.runtimeType); // runtime type = int
-  print(obj.hashCode); // hashcode  number based
-  // on object memory address and vary with JVM variation.
-
-  print(Object.hash("a", 12));
-  print(Object.hashAll([1,2,3]));
 
 }
