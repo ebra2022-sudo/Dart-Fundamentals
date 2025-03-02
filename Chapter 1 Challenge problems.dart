8589eb7cc9df66674e177646615cb741a6bd2af7
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-03-02 10:22:58 +0300

    challenge problem 1 Solution

---
+++ Chapter 1 Challenge problems.dart
@@ -0,0 +1,9 @@
+/// Challenge Problem 1 :
+main() {
+  // initial number of dogs
+  int dogs = 0;
+  // add additional pet
+  print("Initial number of dogs: $dogs.");
+  dogs++;
+  print("Number of  dogs after additional pet: $dogs.");
+}
--- Hello.dart
+++ Hello.dart
@@ -1,13 +1,18 @@
-/// variable in dart
+/// Variable Rules in Dart
+/// Dart is both statically typed and
+/// Dynamically typed using {dynamic} keyword
 
 void main() {
-  /// Difference between Mutable and immutable object
-  /// Mutable objects: these are type of object that
-  ///   -> change their internal state
-  ///   -> sed for date state is variable for some data processing
-  /// Immutable objects: these are type of objects that
-  ///   -> doesn't change its internal state after thy are being initialized
-  ///   -> used for data safety, consistency and thread safe data manipulation
+  /// Variable Declaration
+  /// Dart supports both type annotation and type inference using var or const
+  int a = 10; // type annotation
+  var b = 10;  // type inference with reassigning with the same inferred type
+               // infers based on the value but its type is dynamic until initialization
+  const pi = 3.1416; // type inference  without reassigning and the value must provided value mus be known at compile time,
+  final  k;
+  k = 12;
+  print(k);
+
 
 
 }
