70b1fdb1c09e1da45af7c81824b66a697070a362
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-03-17 17:14:43 +0300

    Overriding the default implementation of the parent class

--- Hello.dart
+++ Hello.dart
@@ -4,11 +4,17 @@
 
 void main() {
   // new keyword is optional in dart
-  var user = User(); // without new, or
-  user = new User(); // with new keyword
-  user.name = "Muhammed";
-  user.id = 1;
-  print(user);
+  User user1 = User();
+  User user2 = User();
+  user1.name = "Muhammed";
+  user1.id = 1;
+  user2.id = 1;
+  user2.name = "Muhammed";
+  print(user1);
+  print(user2);
+  print(user1 == user2);
+
+
 
 }
 
@@ -21,6 +27,14 @@ class User {
   // overriding the toString function of the parent "Object" class
   @override
   String toString() {
-    return "User(${this.id}, ${this.name})";
+    return "User(id : ${this.id}, name : ${this.name})";
+  }
+
+  // overriding the default == operator
+  @override
+  bool operator ==(Object other) {
+    User another = other as User;
+    // TODO: implement ==
+    return this.name == another.name && this.id == another.id;
   }
 }
